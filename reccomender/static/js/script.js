45ce83b293eebcf8eb505f30f204f982e7c594fe
Jeel-4103 <jeel4103@gmail.com>
2024-02-29 00:53:23 +0530

    edit home.html

--- reccomender/static/js/script.js
+++ reccomender/static/js/script.js
@@ -10,4 +10,36 @@ $(document).ready(function(){
     $(".toggler-search-icon").click(function(){
         $(".search").css('display' , "block")
     })
+    $('.movie-content').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
 })
