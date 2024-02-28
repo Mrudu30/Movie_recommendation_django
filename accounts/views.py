c6f726162ebd5c8a41ec9b8ecd4a3cb2e6641398
mrudani <mrudanisongade@gmail.com>
2024-02-28 11:43:04 +0530

    login signup functionality renewed

--- accounts/views.py
+++ accounts/views.py
@@ -1,33 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 from django.db import IntegrityError
+from .forms import UserCreationForm,AuthenticationForm
 
 # Create your views here
-
 def signupaccount(request):
-    if request.method == 'GET':
-        return render(request, 'signupaccount.html', 
-                       {'form':UserCreationForm})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Automatically hashes the password
+            login(request, user)
+            next_url = request.GET.get('next', 'home')  # Redirect to 'next' URL or 'home'
+            return redirect(next_url)
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(request.POST['username'], 
-                            password=request.POST['password1'])
-                user.save()
-                login(request, user)
-                return redirect('home')
-            except IntegrityError:
-                return render(request, 'signupaccount.html', 
-                 {'form':UserCreationForm,
-                 'error':'Username already taken. Choose new username.'})
-        else:
-            return render(request, 'signupaccount.html', 
-             {'form':UserCreationForm, 'error':'Passwords do not match'})
-
+        form = UserCreationForm()
+    return render(request, 'signupaccount.html', {'form': form})
+       
 def logoutaccount(request):        
     logout(request)
     return redirect('home')
