fb77fb7daffde167e96c50dcf6ec8ce923003565
mrudani <mrudanisongade@gmail.com>
2024-02-29 15:16:32 +0530

    forgot password in work

--- reccomender/urls.py
+++ reccomender/urls.py
@@ -3,13 +3,16 @@ from .views import home,index,mylist,detail,filter_movies,deleteComment,editComm
 from accounts import views as av
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',index),
-    path('password-reset/',av.ResetPasswordView.as_view(),name='password-reset'),
+    path('password-reset/',av.ResetPasswordView.as_view(),name='password_reset'),
     path('login/',av.loginaccount,name='login'),
     path('signupaccount/', av.signupaccount, name='signup'),
     path('logout/', av.logoutaccount, name='logout'),
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
     path('home/',home,name='home'),
     path('mylist/',mylist,name='mylist'),
     path('detail/<int:movie_id>/', detail, name='detail'),
